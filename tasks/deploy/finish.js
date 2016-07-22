3319e917ad4c5fcc1e22094a849d0c143b496fd5
Torsten Schmickler <Torsten.schmickler@gmail.com>
2016-07-23 00:59:01 +0300

    Added rollbar deployment notification to shipit

--- tasks/deploy/finish.js
+++ tasks/deploy/finish.js
@@ -12,5 +12,17 @@ module.exports = function (gruntOrShipit) {
   function task() {
     var shipit = init(utils.getShipit(gruntOrShipit));
     shipit.emit('deployed');
+
+    fetch('https://api.rollbar.com/api/1/deploy/', { method: 'POST', body: JSON.stringify({
+      access_token: config.rollbar.api_key,
+      environment: shipit.options.environment,
+      revision: shipit.config.branch,
+      local_username: 'ChangeMeInShipitDeploy'
+      })
+    }).then((res) => {
+     return res.json();
+    }).then((json) => {
+     console.log(json);
+    });
   }
 };

066e6a60b6417009ea8ee98c5785f5ac16aba3ce
Torsten Schmickler <torsten.schmickler@gmail.com>
2016-07-23 14:05:51 +0300

    Send username

--- tasks/deploy/finish.js
+++ tasks/deploy/finish.js
@@ -1,6 +1,6 @@
 var utils = require('shipit-utils');
 var init = require('../../lib/init');
-
+var fetch = require('node-fetch');
 /**
  * Update task.
  * - Emit an event "deployed".
@@ -12,12 +12,12 @@ module.exports = function (gruntOrShipit) {
   function task() {
     var shipit = init(utils.getShipit(gruntOrShipit));
     shipit.emit('deployed');
-
-    fetch('https://api.rollbar.com/api/1/deploy/', { method: 'POST', body: JSON.stringify({
-      access_token: config.rollbar.api_key,
+    fetch('https://api.rollbar.com/api/1/deploy/', {
+        method: 'POST', body: JSON.stringify({
+      access_token: shipit.config.rollbar.api_key,
       environment: shipit.options.environment,
       revision: shipit.config.branch,
-      local_username: 'ChangeMeInShipitDeploy'
+      local_username: process.env.USER
       })
     }).then((res) => {
      return res.json();
